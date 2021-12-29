6325a4b80a252c3b67c345f4ea02861e8c9d8b4c
Cristian Valadez <c_valadez@outlook.com>
2021-12-28 23:29:04 -0600

    Convert from viewpager to viewpager2
    * Viewpager is depracated so converted to utilize viewpager2
    * Deleted most of android studio example to create a simple tab layout
    * Created 2 tabs, one has a button, preparing for java --> flask testing

---
+++ Android_App/app/src/main/java/com/example/msfs2020_cockpit_extension_app/FragmentAdapter.java
@@ -0,0 +1,35 @@
+package com.example.msfs2020_cockpit_extension_app;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class FragmentAdapter extends FragmentStateAdapter {
+
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new ElectricalFrag();
+            case 1:
+                return new PFDFrag();
+        }
+        return new ElectricalFrag();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
--- Android_App/app/src/main/java/com/example/msfs2020_cockpit_extension_app/MainActivity.java
+++ Android_App/app/src/main/java/com/example/msfs2020_cockpit_extension_app/MainActivity.java
@@ -1,45 +1,72 @@
 package com.example.msfs2020_cockpit_extension_app;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import androidx.viewpager.widget.ViewPager;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.msfs2020_cockpit_extension_app.ui.main.SectionsPagerAdapter;
 import com.example.msfs2020_cockpit_extension_app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager2);
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
+        FragmentManager fm = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fm, getLifecycle());
+        viewPager2.setAdapter(fragmentAdapter);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        // Add the tabs
+        tabLayout.addTab(tabLayout.newTab().setText(TAB_TITLES[0]));
+        tabLayout.addTab(tabLayout.newTab().setText(TAB_TITLES[1]));
+
+        // Tab selected listener
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(@NonNull TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
+
+        // Swipe page callback
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+
+    public void toggleLandingLights(View view){
+        System.out.println("Button Pressed!");
+        Snackbar.make(findViewById(R.id.view_pager2), "Button Pressed!", Snackbar.LENGTH_SHORT)
+                .show();
     }
 }
