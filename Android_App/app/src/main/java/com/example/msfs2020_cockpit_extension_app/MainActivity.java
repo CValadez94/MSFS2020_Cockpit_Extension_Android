2290adddaf96a7543b65f9532ebec08c7a6088e4
Cristian Valadez <c_valadez@outlook.com>
2021-12-29 16:28:26 -0600

    Add method to android app to send http POST to flask server
    * Used OkHTTP to send an http POST to toggle landing lights

--- Android_App/app/src/main/java/com/example/msfs2020_cockpit_extension_app/MainActivity.java
+++ Android_App/app/src/main/java/com/example/msfs2020_cockpit_extension_app/MainActivity.java
@@ -12,6 +12,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.msfs2020_cockpit_extension_app.databinding.ActivityMainBinding;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     FragmentAdapter fragmentAdapter;
+    private TriggerEvent triggerValue = new TriggerEvent();     // Default value is 1
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
 
@@ -62,11 +90,83 @@ public class MainActivity extends AppCompatActivity {
                 tabLayout.selectTab(tabLayout.getTabAt(position));
             }
         });
+
+        viewPager2.setUserInputEnabled(false);          // Disable the swiping feature
     }
 
-    public void toggleLandingLights(View view){
+    // Send a POST to the flask server
+    private void postToFlaskServer(String file) throws Exception {
+        MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+        String url = "http://192.168.50.39:5000/event/LANDING_LIGHTS_TOGGLE/trigger";
+        String json = "{\"value_to_use\":\"1\"}";
+        byte[] postData = json.getBytes(StandardCharsets.UTF_8);
+
+
+
+        RequestBody requestBody = RequestBody.create(new byte[0], null);
+        requestBody = RequestBody.create(json, JSON);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                System.out.print("FAILURE!");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException(("Unexpected code " + response));
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + " : " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(responseBody.string());
+                }
+            }
+        });
+    }
+
+    public void toggleLandingLights(View view) {
         System.out.println("Button Pressed!");
         Snackbar.make(findViewById(R.id.view_pager2), "Button Pressed!", Snackbar.LENGTH_SHORT)
                 .show();
+
+        try {
+            postToFlaskServer("/event/LANDING_LIGHTS_TOGGLE/trigger");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Exception caught.");
+        }
+    }
+
+    class TriggerEvent {
+        private int value;
+
+        // Constructors
+        public TriggerEvent(int value) {
+            this.value = value;
+        }
+
+        // No param will default to value 1
+        public TriggerEvent() {
+            this(1);
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
     }
 }
